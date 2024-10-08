5eb37aa7b7d33a8ac01541facfdb1c0cb170f8ac
Lucas Kennde <166322461+LucasKennde@users.noreply.github.com>
2024-10-08 19:04:22 -0300

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,42 @@
-import './style.css'
+const lampada = document.querySelector<HTMLImageElement>("#lampada")
+const botaoLampada = document.querySelector("#botao-lampada")
+let tomada: boolean = false
+let lampadaQuebrada: boolean = false
 
-const button = document.querySelector<HTMLButtonElement>('#button')
-const nome = document.querySelector<HTMLInputElement>('#nome')
-const email = document.querySelector<HTMLInputElement>('#email')
-const idade = document.querySelector<HTMLInputElement>('#idade')
-const password = document.querySelector<HTMLInputElement>('#password')
-
-
-if (button) {
-  button.addEventListener('click', () => {
-    if (nome && email && idade && password) {
-      if (!nome.value || !email.value || !idade.value || !password.value) {
-        alert('Preencha todos os campos')
-        return
-      }
-      if (!email.value.includes('@') || !email.value.split('@')[1].includes('.')) {
-        alert('Email não confere')
-        return
-      }
-      if (isNaN(Number(idade.value)) && Number(idade.value) > 0) {
-        alert('Idade deve ser um número e ser maior que zero')
-        return
-      }
-      if (password.value.length < 8) {
-        alert('Senha deve ter pelo menos 8 caracteres')
-        return
-      }
-      alert('Cadastro realizado com sucesso!')
-      const user = {
-        nome: nome.value,
-        email: email.value,
-        idade: idade.value,
-        password: password.value
-      }
-      console.log(user)
-    }
-  })
+function getNumeroAleatorio(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function verificarNumero(number: number): boolean {
+  if (number == 5) {
+    return false
+  }
+  return true
+}
+
+
+if (lampada && botaoLampada) {
+  botaoLampada.addEventListener('click', () => {
+
+    const numeroAleatorio: number = getNumeroAleatorio(1, 10)
+
+    if (verificarNumero(numeroAleatorio)) {
+      lampada.src = "/quebrada.png"
+      lampadaQuebrada = !lampadaQuebrada
+    }
+
+
+    if (!lampadaQuebrada) {
+      if (tomada) {
+        lampada.src = "/desligada.png"
+        tomada = !tomada
+      } else {
+        lampada.src = '/ligada.png'
+        tomada = !tomada
+      }
+    }
+    
+  })
+
+}
 
