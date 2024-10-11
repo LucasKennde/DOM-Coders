aeae49dcb856899ac1b94f85807ee8aff9f015cb
Guilherme <guigamf@hotmail.com>
2024-10-11 00:31:10 -0300

    Adicionado validação do resultado

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,46 @@
-import "./style.css"
-const formulario = document.querySelector<HTMLFormElement>('#formulario')
-const resultado: any = document.querySelector<HTMLDivElement>('.resultado')
-const inputTarefas = document.querySelector<HTMLInputElement>('#inputTarefas')
+import "./style.css";
+const formulario = document.querySelector<HTMLFormElement>('#formulario');
+const resultado = document.querySelector<HTMLDivElement>('.resultado');
+const inputTarefas = document.querySelector<HTMLInputElement>('#inputTarefas');
 let tarefas: string[] = JSON.parse(localStorage.getItem('tarefas') || '[]');
 
 
 
 function mostrarResultado() {
-  resultado.innerHTML = ''
-  tarefas.forEach((tarefa: string, index: number) => {
-    const li = document.createElement('li')
-    li.textContent = tarefa
-    const botaoExcluir = document.createElement('button')
-    botaoExcluir.textContent = 'Excluir'
-    botaoExcluir.addEventListener('click', () => {
-      removerTarefa(index)
-    })
-    li.appendChild(botaoExcluir)
-    resultado.appendChild(li)
-  })
+  if(resultado) {
+    resultado.innerHTML = '';
+    tarefas.forEach((tarefa: string, index: number) => {
+      const li = document.createElement('li');
+      li.textContent = tarefa;
+      const botaoExcluir = document.createElement('button');
+      botaoExcluir.textContent = 'Excluir';
+      botaoExcluir.addEventListener('click', () => {
+        removerTarefa(index);
+      });
+      li.appendChild(botaoExcluir);
+      resultado.appendChild(li);
+    });
+  }
 }
 
 
 function removerTarefa(index: number) {
-  tarefas.splice(index, 1)
-  localStorage.setItem('tarefas', JSON.stringify(tarefas))
-  mostrarResultado()
+  tarefas.splice(index, 1);
+  localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  mostrarResultado();
 }
 
 
 if (formulario && inputTarefas) {
 
   formulario.addEventListener('submit', (e) => {
-    e.preventDefault()
-    tarefas.push(inputTarefas.value)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
-    console.log(tarefas)
-    mostrarResultado()
-    formulario.reset()
+    e.preventDefault();
+    tarefas.push(inputTarefas.value);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    console.log(tarefas);
+    mostrarResultado();
+    formulario.reset();
   })
 }
 
-mostrarResultado()
+mostrarResultado();
